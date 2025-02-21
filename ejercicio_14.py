9f104e00867b4a861c7770c268a2a0c9befd4206
josue <michaelfb803@gmail.com>
2025-02-21 11:07:16 -0500

    confirma14

---
+++ ejercicio_14.py
@@ -0,0 +1,5 @@
+# Simula el funcionamiento de una máquina expendedora creando una operación que reciba dinero de monedas y un número que indique la selección del producto.
+#▪ El programa retornará el nombre del producto y el dinero de vuelta (con el menor número de monedas).
+#▪ Si el dinero es insuficiente o el número de producto no existe, deberá indicarse con un mensaje y retornar todas las monedas. Si no hay dinero de vuelta, se retornará cero.
+#▪ Para que resulte más simple, trabajaremos en céntimos con monedas 5, 10, 50, 100 y 200.
+#▪ Se debe controlar que las monedas enviadas estén dentro de las soportadas.
