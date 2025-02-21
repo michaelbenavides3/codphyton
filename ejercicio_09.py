54cbcc6df8162f00875e0e539aec318f8af6fe29
josue <michaelfb803@gmail.com>
2025-02-21 08:04:42 -0500

    confirma9

--- ejercicio_09.py
+++ ejercicio_09.py
@@ -1,19 +1,23 @@
 # Para implementar un reloj digital es necesario declarar tres variables, para controlar: horas, minutos y segundos. Cada una de estas variables controla un ciclo, así: los segundos se incrementan desde 0 a 59, los minutos también desde 0 a 59 y las horas desde 1 a 12 o de 1 a 24. Este algoritmo está diseñado para terminar la ejecución al llegar a 12:59:59
 
+#importar time
+
+import time
+
 #declaramos variables 
 horas = 1
 minutos = 0
-segudnos = 0
+segundos = 0
 
 while horas <= 12:
     while minutos <=59:
-        while segudnos <=59:
-            print(f"{horas:02d}:{minutos:02d}:{segudnos:02d}")
-            segudnos += 1
+        while segundos <=59:
+            print(f"{horas}:{minutos}:{segundos}")
+            segundos += 1
         minutos += 1
-        segudnos += 0
+        segundos += 0 #reiniciar segundo al cambiar de minutos
     horas += 1
-    minutos = 0
+    minutos = 0 #reiniciar minutos al cambiar de hora
             
             
             
